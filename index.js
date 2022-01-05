f81f17adf939bf5f94fdca35a0484320509fb17b
David Poza Suárez <pozasuarez@gmail.com>
2022-01-05 19:21:03 +0100

    working

--- index.js
+++ index.js
@@ -4,147 +4,6 @@ const slugify = require('slugify');
 
 const server = 'http://127.0.0.1:41184/';
 
-/**
- * Search tag id given tag title
- * @param {string} tagTitle existing tag title
- * @return {string} existing tag id
- */
-function searchTagId(tagTitle) {
-  const url = [
-    server,
-    'search?',
-    'token=',
-    process.env.API_KEY,
-    '&query=',
-    tagTitle,
-    '&type=',
-    'tag',
-  ].join('');
-
-  return (
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log('searching tag id for tag title: ', tagTitle);
-        if (data && data.length >= 1) {
-          const tagId = data[0].id;
-          console.log('> result: ', tagId);
-          return (Promise.resolve(tagId));
-        }
-        return (Promise.reject('Error: tag ', tagTitle, ' not found.'));
-      })
-  );
-}
-
-/**
- * Creates a tag with name specified.
- * @param {string} title original note title
- * @return {string} new tag id
- */
-function createTag(title) {
-  const url = [
-    server,
-    'tags?',
-    'token=',
-    process.env.API_KEY,
-  ].join('');
-  const tagName = slugify(title, { remove: /[*+~.():;'"!@]/g, lower: true});
-
-  return (
-    fetch(url, {
-      method: 'POST',
-      headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        title: tagName,
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log('creating tag with name: ', tagName);
-        if (data.id) {
-          console.log('and id:', data.id);
-          return (Promise.resolve(data.id));
-        } else if (data.error.includes('already exists')) {
-          // look for the tag id using search endpoint
-          return (searchTagId(tagName))
-            .then((tagId) => {
-              return (Promise.resolve(tagId));
-            })
-        }
-        return (Promise.reject('Error creating tag'));
-      })
-  );
-}
-
-/**
- * Assigns given tag to given note.
- * @param {string} tagId
- * @param {string} nodeId
- * @param {string} noteTitle for log
- */
-function assignTagToNote(tagId, noteId, noteTitle) {
-  const url = [
-    server,
-    'tags/',
-    tagId,
-    '/notes',
-    '?token=',
-    process.env.API_KEY,
-  ].join('');
-
-  return (
-    fetch(url, {
-      method: 'POST',
-      headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        id: noteId,
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(`* assigning tag (${tagId}) to note ${noteTitle} (${noteId})`);
-      })
-  );
-}
-
-/**
- * Returns folder id to given folder title.
- * @param {string} folderTitle
- * @return {string} folder id
- */
-function getFolderId(folderTitle) {
-  const url = [
-    server,
-    'search?',
-    'token=',
-    process.env.API_KEY,
-    '&query=',
-    `*${folderTitle}*`, // use wildcard just to catch title with icons
-    '&type=',
-    'folder',
-  ].join('');
-
-  return (
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log('searching folder title for title: ', folderTitle);
-        if (data && data.length >= 1) {
-          const folderId = data[0].id;
-          console.log('> result: ', folderId);
-          return (Promise.resolve(folderId));
-        }
-        return (Promise.reject('Error: folder ', folderTitle, ' not found.'));
-      })
-  );
-}
-
 /*returns a string like "#tag1 #complex-tag #another-tag" */
 async function getNoteTags(noteId) {
   const url = [
@@ -157,7 +16,7 @@ async function getNoteTags(noteId) {
   const data = await res.json();
   if (!data.items) return '';
   return data.items.reduce((prev, curr) => {
-    return (`${prev} #${curr.title.replace(/ /g, '-')}`);
+    return (`${prev} #${slugify(curr.title)}`);
   }, '');
 }
 
@@ -166,26 +25,65 @@ async function getNoteTags(noteId) {
  * If not folderId is provided then fetches all notes.
  * @param {string} folderId
  */
-async function fetchAllNotes(folderId) {
+async function getAllNotes(folderId, page) {
   const url = [
     server,
     folderId ? `folders/${folderId}/notes?` : 'notes?',
     'token=',
     process.env.API_KEY,
-    '&fields=id,body',
+    '&fields=id,body,title',
     '&limit=100',
-    '&page=3'
+    `&page=${page}`
   ].join('');
 
   const res = await fetch(url);
   const data = await res.json();
-  return data.items;
+
+  return data;
+}
+
+function updateNoteBody(noteId, newBody) {
+  const url = [
+    server,
+    `notes/${noteId}`,
+    '?token=',
+    process.env.API_KEY,
+  ].join('');
+
+  return fetch(url, {
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    method: 'PUT',
+    body: JSON.stringify({
+      body: newBody
+    })
+  });
 }
 
 (async () => {
-  const notes = await fetchAllNotes();
-  notes.forEach(async (note) => {
-    const tags = await getNoteTags(note.id);
-    console.log(tags);
-  });
+  let total = 0;
+  let pageNum = 1;
+  let res = { items: [], has_more: true };
+  do {
+    console.log(`retrieving page: ${pageNum}`);
+    res = await getAllNotes(null, pageNum);
+    total += res.items.length;
+		res.items.forEach(async (note) => {
+      const oldBody = note.body;
+      const tags = await getNoteTags(note.id);
+      const newBody = `${tags}\n${oldBody}`;
+      if (tags) console.log(`${note.title}, adding ${tags}`);
+      try {
+        await updateNoteBody(note.id, newBody);
+      } catch (err) {
+        console.log(err);
+        return;
+      }
+    });
+    pageNum++;
+	} while (res.has_more);
+  console.log(`SUCCESS!. ${total} notes were tagged.`)
 })();
+
